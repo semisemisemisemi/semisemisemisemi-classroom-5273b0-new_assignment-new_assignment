d3ff246ba891960da18ca4e198dcdbc8fe15039e
leedyun <105830643+leedyun@users.noreply.github.com>
2024-07-29 01:23:11 +0900

    correct code

--- student_code.cpp
+++ student_code.cpp
@@ -1,1 +1,17 @@
-// 여기에 작성해주세요
+#include<iostream>
+
+int main()
+{
+	int num;
+	int sum = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << i + 1 << "번째 정수 입력: ";
+		std::cin >> num;
+		sum += num;
+	}
+	std::cout << "합계: " << sum << std::endl;
+
+	return 0;
+}
